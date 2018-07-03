f5690f995ecf615db4ccbbed5824471ef1ac38ac
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2018-07-03 15:04:25 +0530

    authentication

--- src/Chatter/Middleware/Authentication.php
+++ src/Chatter/Middleware/Authentication.php
@@ -6,19 +6,23 @@ use Chatter\Models\User;
 
 class Authentication
 {
+
     public function __invoke($request, $response, $next)
     {
         $auth = $request->getHeader('Authorization');
         $_apikey = $auth[0];
-        $apikey = substr($_apikey, strpos($_apikey, ' ') + 1);
+        $apikey = substr($_apikey, strpos($_apikey, ' '));
+        $apikey = trim($apikey);
 
         $user = new User();
+
         if (!$user->authenticate($apikey)) {
             $response->withStatus(401);
 
             return $response;
         }
 
+        $request = $request->withAttribute('user_id', $user->id);
         $response = $next($request, $response);
 
         return $response;
